39a31ab1a2752a446e2ebf3c819371a5237977e4
Hoang Linh <hlinh.jobs@gmail.com>
2023-08-22 11:14:00 +0700

    init project

---
+++ MusicApp/MusicApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Hoang Linh Nguyen on 22/8/2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
