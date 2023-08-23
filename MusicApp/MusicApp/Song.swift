35a5bd97dbd7d0b273b51523d837937a7f366589
Hoang Linh <hlinh.jobs@gmail.com>
2023-08-23 17:46:12 +0700

    Build UIContainerView

---
+++ MusicApp/MusicApp/Song.swift
@@ -0,0 +1,14 @@
+//
+//  Song.swift
+//  MusicApp
+//
+//  Created by Hoang Linh Nguyen on 24/8/2023.
+//
+
+import Foundation
+
+struct Song {
+    var songImage : String
+    var songName: String
+    var songArtist: String
+}
--- MusicApp/MusicApp/ViewController.swift
+++ MusicApp/MusicApp/ViewController.swift
@@ -8,19 +8,27 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    
+    @IBOutlet private weak var songImage: UIImageView!
+    @IBOutlet private weak var songName: UILabel!
+    @IBOutlet private weak var songArtist: UILabel!
     @IBOutlet private weak var playButton: UIButton!
-    private var isPlaying : Bool = false
+    
+    private var isPlaying = false
+    var selectedSong : Song?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let selectedSong = selectedSong{
+            songImage.image = UIImage(named: selectedSong.songImage)
+            songName.text = selectedSong.songName
+            songArtist.text = selectedSong.songArtist
+        }
     }
     
     @IBAction private func playButtonTapped(_ sender: UIButton) {
         var imageSystemName : String
         isPlaying ? (imageSystemName = "play.circle.fill") : (imageSystemName = "pause.circle.fill")
         isPlaying = !isPlaying
-
         sender.setImage(UIImage(systemName: imageSystemName), for: .normal)
     }
 }
