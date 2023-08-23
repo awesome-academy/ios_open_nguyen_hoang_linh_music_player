6b556e650ba51804540b603670198bc57c43d0dc
Hoang Linh <hlinh.jobs@gmail.com>
2023-08-23 10:28:39 +0700

    Build CommandCenter Controller

---
+++ MusicApp/MusicApp/Song.swift
@@ -0,0 +1,15 @@
+//
+//  Song.swift
+//  MusicApp
+//
+//  Created by Hoang Linh Nguyen on 24/8/2023.
+//
+
+import Foundation
+
+struct Song {
+    var songImage : String
+    var songName: String
+    var songArtist: String
+    var songURL: String
+}
