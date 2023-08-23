a8f4a44a9898e5784e0efd6f8d7817f0d8b1246d
Hoang Linh <hlinh.jobs@gmail.com>
2023-08-23 10:28:39 +0700

    Build UIControls

--- MusicApp/MusicApp/ViewController.swift
+++ MusicApp/MusicApp/ViewController.swift
@@ -8,8 +8,20 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var playButton: UIButton!
+    private var isPlaying : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction private func playButtonTapped(_ sender: UIButton) {
+        var imageSystemName : String
+        isPlaying ? (imageSystemName = "play.circle.fill") : (imageSystemName = "pause.circle.fill")
+        isPlaying = !isPlaying
+
+        sender.setImage(UIImage(systemName: imageSystemName), for: .normal)
     }
 }
 
