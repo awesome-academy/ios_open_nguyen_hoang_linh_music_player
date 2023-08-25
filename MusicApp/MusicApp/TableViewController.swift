60efcafbab8779adffb442cdd295fd93d15cbfb8
Hoang Linh <hlinh.jobs@gmail.com>
2023-08-25 15:08:41 +0700

    Build CommandCenter Controller

--- MusicApp/MusicApp/TableViewController.swift
+++ MusicApp/MusicApp/TableViewController.swift
@@ -10,51 +10,43 @@ import UIKit
 final class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet private weak var songTableView: UITableView!
     private var songList = [Song]()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         songTableView.dataSource = self
         songTableView.delegate = self
         initSongList()
     }
-    
+
     func initSongList(){
-        let songs = [Song(songImage: "album1", songName: "Album 1", songArtist: "Ngọt"),
-                     Song(songImage: "album2", songName: "Album 2", songArtist: "Ngọt"),
-                     Song(songImage: "album3", songName: "Album 3", songArtist: "Ngọt"),
-                     Song(songImage: "album4", songName: "Album 4", songArtist: "Ngọt"),
-                     Song(songImage: "album1", songName: "Album 1", songArtist: "Ngọt"),
-                     Song(songImage: "album2", songName: "Album 2", songArtist: "Ngọt"),
-                     Song(songImage: "album3", songName: "Album 3", songArtist: "Ngọt"),
-                     Song(songImage: "album4", songName: "Album 4", songArtist: "Ngọt"),
-                     Song(songImage: "album1", songName: "Album 1", songArtist: "Ngọt"),
-                     Song(songImage: "album2", songName: "Album 2", songArtist: "Ngọt"),
-                     Song(songImage: "album3", songName: "Album 3", songArtist: "Ngọt"),
-                     Song(songImage: "album4", songName: "Album 4", songArtist: "Ngọt")]
-        
+        let songs = [Song(songImage: "album1", songName: "Album 1", songArtist: "Ngọt", songURL: "album1"),
+                     Song(songImage: "album2", songName: "Album 2", songArtist: "Ngọt", songURL: "album2"),
+                     Song(songImage: "album3", songName: "Album 3", songArtist: "Ngọt", songURL: "album3"),
+                     Song(songImage: "album4", songName: "Album 4", songArtist: "Ngọt", songURL: "album4")]
+
         _ = songs.map { songList.append($0) }
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return songList.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "tableViewCellId") as? TableViewCell else
         {
             let cell = UITableViewCell(style: .default, reuseIdentifier: "tableViewCellId")
             return cell
         }
-        
+
         let thisSong = songList[indexPath.row]
         tableViewCell.config(thisSong: thisSong)
         return tableViewCell
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.performSegue(withIdentifier: "detailSegue", sender: self)
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == "detailSegue"){
             let indexPath = self.songTableView.indexPathForSelectedRow!
--- MusicApp/MusicApp/ViewController.swift
+++ MusicApp/MusicApp/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
 
 final class ViewController: UIViewController {
     @IBOutlet private weak var songImage: UIImageView!
@@ -13,8 +15,14 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var songArtist: UILabel!
     @IBOutlet private weak var playButton: UIButton!
     
+    private var audioPlayer: AVPlayer?
+    private var playerItem: AVPlayerItem?
+    private var songURL : URL?
     private var isPlaying = false
     var selectedSong : Song?
+    private let commandCenter = MPRemoteCommandCenter.shared();
+    private var commandPlay: Any?
+    private var commandPause: Any?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +30,81 @@ final class ViewController: UIViewController {
             songImage.image = UIImage(named: selectedSong.songImage)
             songName.text = selectedSong.songName
             songArtist.text = selectedSong.songArtist
+            if let path = Bundle.main.path(forResource: selectedSong.songURL, ofType:"mp3") {
+                songURL = URL(filePath: path)
+            }
+        }
+        
+        if let songURL = songURL {
+            playerItem = AVPlayerItem(url: songURL)
+            audioPlayer = AVPlayer(playerItem: playerItem)
+            setupAudioSession()
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nil
+        commandCenter.playCommand.isEnabled = false
+        commandCenter.playCommand.removeTarget(commandPlay)
+        commandCenter.pauseCommand.isEnabled = false
+        commandCenter.pauseCommand.removeTarget(commandPause)
+    }
+    
+    private func setupAudioSession() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [])
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Error setting the AVAudioSession:", error.localizedDescription)
         }
     }
     
     @IBAction private func playButtonTapped(_ sender: UIButton) {
         var imageSystemName : String
-        isPlaying ? (imageSystemName = "play.circle.fill") : (imageSystemName = "pause.circle.fill")
-        isPlaying = !isPlaying
+        isPlaying ? (
+            imageSystemName = "play.circle.fill",
+            self.audioPlayer?.pause()
+        ) : (
+            imageSystemName = "pause.circle.fill",
+            self.audioPlayer?.play()
+        )
+        setupNowPlaying()
+        setupRemoteCommandCenter()
+        isPlaying.toggle()
         sender.setImage(UIImage(systemName: imageSystemName), for: .normal)
+    }
+    
+    
+    private func setupNowPlaying() {
+        var nowPlayingInfo = [String: Any]()
+        if let selectedSong = selectedSong {
+            nowPlayingInfo[MPMediaItemPropertyTitle] = selectedSong.songName
+            nowPlayingInfo[MPMediaItemPropertyArtist] = selectedSong.songArtist
+            if let image = UIImage(named: selectedSong.songImage){
+                nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: image.size, requestHandler: { (size) -> UIImage in
+                    return image
+                })
+            }
+        }
+        nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = playerItem?.currentTime().seconds
+        nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = playerItem?.asset.duration.seconds
+        nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = audioPlayer?.rate
+        
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+        MPNowPlayingInfoCenter.default().playbackState = .playing
+    }
+    
+    private func setupRemoteCommandCenter() {
+        commandCenter.playCommand.isEnabled = true
+        commandPlay = commandCenter.playCommand.addTarget {event in
+            self.audioPlayer?.play()
+            return .success
+        }
+        commandCenter.pauseCommand.isEnabled = true
+        commandPause = commandCenter.pauseCommand.addTarget {event in
+            self.audioPlayer?.pause()
+            return .success
+        }
     }
 }
 
